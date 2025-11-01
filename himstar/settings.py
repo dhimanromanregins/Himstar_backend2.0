850ebda21dc5e029097c33f073974e71b29b26f4
Sahilcrossml <sahil.dhiman@crossml.com>
2025-11-01 10:27:21 +0530

    updated email creds

--- himstar/settings.py
+++ himstar/settings.py
@@ -201,8 +201,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'himstar98@gmail.com'
-EMAIL_HOST_PASSWORD = 'lqedaxcennylorfs'
+EMAIL_HOST_USER = 'Sahildhiman98765@gmail.com'
+EMAIL_HOST_PASSWORD = 'hrdrpqtvptxdtwll'
 
 
 # Azure Details
